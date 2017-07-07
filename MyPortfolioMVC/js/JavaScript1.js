0382e9a11f9f9a6b0868009dfe0585d8a7b80b96
Tim Nguyen <timdung_tim@yahoo.com>
2017-07-07 16:52:26 -0400

    Initial commit

---
+++ MyPortfolioMVC/js/JavaScript1.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+
+    $('#fbcode').hide();
+
+    $('#btnfbcode').click(function () {
+        $('#fbcode').toggle();
+    });
+    
+   $('#palindromeCode').hide();
+
+    $('#factorialCode').hide();
+
+    $('#numbersCode').hide();
+
+    $('[data-toggle="popover"]').popover()
+
+    //sure. This is the file in MVC when i clicked on that view code in mvc and it dose not work like the html page =, sir
+    +//Ok. I am going to just take a look around
+
+    $('#btnPalindrome').click(function () {
+        $('#palindromeCode').toggle();
+    })
+
+    $('#btnFactorial').click(function () {
+        $('#factorialCode').toggle();
+    })
+
+    $('#btnNumbers').click(function () {
+        $('#numbersCode').toggle();
+    })
+});
+
---
+++ MyPortfolioMVC/js/app.js
@@ -0,0 +1,124 @@
+﻿    $("#fizzBuzz1").click(function () {
+        var array = []; arr = [];
+        $("#fb input").each(function () {
+            array.push(Number($(this).val()));
+        })
+        for (var i = 1; i <= 100; i++) {
+            if (i % array[0] == 0 && i % array[1] == 0) {
+                arr.push("fizzbuzz");
+            }
+
+            else if (i % array[0] == 0) {
+                arr.push("fizz");
+            }
+
+            else if (i % array[1] == 0) {
+                arr.push("buzz");
+            }
+
+            else {
+                arr.push(i);
+            }
+        }
+        //var arr = arr.toString();
+        var arr = arr.join(", ");
+        $("#answerFizzBuzz").html(arr);
+    });
+
+    $("#btnClear").click(function () {
+        $(".form-horizontal1 input, .form-horizontal1 #answerFizzBuzz").each(function () {
+            $(this).val("");
+            $(this).empty();
+        })
+    })
+
+
+
+    //Palindrome Function
+
+    $("#submit").click(function () {
+        var str = $("#typeWords").val();
+        str = str.toLowerCase();
+        str = str.replace(/[\W_]/g, "");
+        var inputStr1 = str.split("").reverse().join("");
+
+        if (inputStr1 == str) {
+            $("#answer").html("Yes, " + str + " IS a palindrome!")
+        }
+        else {
+            $("#answer").html("No, " + str + " IS NOT a palindrome!")
+        }
+
+    });
+
+    $("#btnClearPal").click(function () {
+        $("#typeWords, #answer ").each(function () {
+            $(this).val("");
+            $(this).html("");
+        })
+    })
+
+    //End Palindrome function
+
+
+    //Start off factorialFunc
+
+    $("#result").click(function () {
+        var num = parseInt($("#inputNumber").val());
+        if (num == 0) {
+            return document.getElementById("answer1").innerHTML = 1;
+        }
+        else if (/\./.test(num)) {
+            $("#answer1").html("Please enter an integer number");
+        }
+        else if (num > 150) {
+            $("#answer1").html("The number you have entered is too large. Please keep it between 1 and 150.");
+        }
+        else {
+            var number = 1;
+            for (var i = num; i > 0; i--) {
+                number *= i;
+            }
+        }
+        $("#answer1").html("The factorial number is: " + number);
+    });
+
+    $("#cls").click(function () {
+        $("#inputNumber, #answer1").each(function () {
+            $(this).val("");
+            $(this).empty();
+        })
+    })
+
+    //End of Factorial function
+
+    //Start of function Number App
+
+    $("#linkOfCal").click(function () {
+        var numArray = [], total = 0, i = 0; product = 1;
+        $("#wholeNumber input").each(function () {
+            i++;
+
+            numArray.push($(this).val());
+            total += parseInt($(this).val());
+            mean = total / i;
+            product *= (parseInt($(this).val()));
+        })
+        var resultMax = Math.max.apply(Math, numArray)
+        var resultMin = Math.min.apply(Math, numArray)
+        $("#output1").html("The maximum number is: " + resultMax);
+        $("#output2").html("The minimum number is: " + resultMin);
+        $("#output3").html("The summation is: " + total);
+        $("#output4").html("The mean is: " + mean)
+        $("#output5").html("So product of all numbers: " + product);
+    });
+
+    $("#clr1").click(function () {
+        $("#wholeNumber input, #manyOutputs p").each(function () {
+            $(this).val("");
+            $(this).empty();
+        })
+    });
+
+
+    //End of function Number App
